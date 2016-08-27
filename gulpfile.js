70c66f29b77b2feb255188d54ff76468ac9d5c6a
Emir Marques <emirdeliz@gmail.com>
2016-08-26 23:04:45 -0300

    Ajuste rotina de testes

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,7 @@ var argv = require('minimist')(process.argv.slice(2));
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-
-var gprotractor = require('gulp-protractor');
+//var karma = require('karma').server;
 var angularProtractor = require('gulp-angular-protractor');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
@@ -15,9 +14,6 @@ var webserver = require('gulp-webserver');
 var mainBowerFiles = require('gulp-main-bower-files');
 var gulpFilter = require('gulp-filter');
 var inject = require('gulp-inject');
-
-var webdriver_standalone = gprotractor.webdriver_standalone;
-var webdriver_update = gprotractor.webdriver_update;
 
 gulp.task('build-js', function () {
     gulp.src(buildConfig.jsFiles)
@@ -86,27 +82,25 @@ gulp.task('watch', ['build'], function () {
 
 gulp.task('webserver', function() {
     gulp.src('build')
-        .pipe(webserver({
-            livereload: true,
-            directoryListing: false,
-            open: false
+    .pipe(webserver({
+        livereload: true,
+        directoryListing: false,
+        open: false
     }));
 });
 
-gulp.task('webdriver_update', webdriver_update);
-gulp.task('webdriver_standalone', webdriver_standalone);
-
 // kill -9 $(lsof -ti tcp:4444)
-gulp.task('test', ['webdriver_update'], function(done) {
+gulp.task('test', ['webserver'], function(done) {
     gulp.src(['./test/vehicle.navigation.spec.js', './test/vehicle.save.spec.js'])
 	   .pipe(angularProtractor({
 		configFile: 'protractor.config.js',
-		autoStartStopServer: false,
+		args: ['--baseUrl', 'http://127.0.0.1:8000'],
+		autoStartStopServer: true,
 		debug: false
 	})).on('error', function(e) {
         console.log('Error: ' + e.message);
         throw e;
-    }).on('end', done);
+    });
 });
 
 gulp.task('check-jshint', function (){
--- protractor.config.js
+++ protractor.config.js
@@ -1,4 +1,5 @@
 exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['test/vehicle.navigation.spec.js', 'test/vehicle.save.spec.js'],
     capabilities: {
         browserName: 'chrome',
