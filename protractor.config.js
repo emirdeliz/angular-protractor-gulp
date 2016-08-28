ddd2e8761ae456a508fb49a8e5a1fb88c0d06fe9
Emir Marques <emirdeliz@gmail.com>
2016-08-27 22:00:46 -0300

    Teste protractor

--- protractor.config.js
+++ protractor.config.js
@@ -1,15 +1,10 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['test/vehicle.navigation.spec.js', 'test/vehicle.save.spec.js'],
-    baseUrl: 'http://localhost:8080',
     rootElement: 'html',
     capabilities: {
         browserName: 'chrome',
-        shardTestFiles: true,
+    	shardTestFiles: true,
         maxInstances: 2
-    },
-    maxSessions: 1,
-    allScriptsTimeout: 40000,
-    getPageTimeout: 40000,
-    rootElement: 'html'
+    }
 }
