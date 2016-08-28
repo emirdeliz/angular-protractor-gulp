e765c71d72d770ef09363e25e4a2c53c60aaf9e4
Emir Marques <emirdeliz@gmail.com>
2016-08-27 21:41:04 -0300

    Teste travis

--- protractor.config.js
+++ protractor.config.js
@@ -1,6 +1,8 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['test/vehicle.navigation.spec.js', 'test/vehicle.save.spec.js'],
+    baseUrl: 'http://localhost:8080',
+    rootElement: 'html',
     capabilities: {
         browserName: 'chrome',
     	shardTestFiles: true,
