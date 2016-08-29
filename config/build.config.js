f03fde363031a4c50940acac309cef7fa2c8032d
Emir Marques <emirdeliz@gmail.com>
2016-08-28 21:42:08 -0300

    Ajuste path build gulp

--- config/build.config.js
+++ config/build.config.js
@@ -4,7 +4,7 @@ module.exports = {
     fileNameVendor: 'vendor.js',
     fileNameJs: 'bundle.js',
     fileNameCss: 'style.css',
-    dist: 'build',
+    dist: __dirname.substring(0, __dirname.indexOf('config')) + 'build',
     sassFiles: ['src/**/*.sass'],
     jsFiles: ['src/**/*.js', './bower_components/angular-i18n/angular-locale_pt-br.js'],
     testFiles: ['src/test/**/*.js'],
--- gulpfile.js
+++ gulpfile.js
@@ -56,6 +56,9 @@ gulp.task('build', function(callback) {
 });
 
 gulp.task('inject', function () {
+
+    console.log('buildConfig.dist ' + buildConfig.dist)
+
     var target = gulp.src('./src/index.html');
     var sources = gulp.src([
         buildConfig.dist + '/' + buildConfig.fileNameVendor,
